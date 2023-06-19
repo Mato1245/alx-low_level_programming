ca221be7cda177ef8196137d029a7beb09ac4da6
Mato Adrian <adriangekara8@gmail.com>
2023-06-19 13:40:22 -0700

    Write a program that prints all possible combinations of two two-digit numbers.
    The numbers should range from 0 to 99
    The two numbers should be separated by a space
    All numbers should be printed with two digits. 1 should be printed as 01
    The combination of numbers must be separated by comma, followed by a space
    The combinations of numbers should be printed in ascending order
    00 01 and 01 00 are considered as the same combination of the numbers 0 and 1
    You can only use the putchar function (every other function (printf, puts, etc…) is forbidden)
    You can only use putchar eight times maximum in your code
    You are not allowed to use any variable of type char
    All your code should be in the main function

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,31 +1,41 @@
-#include <stdio.h>
-
+#include<stdio.h>
+#include<unistd.h>
 /**
-* main - Program that prints all possible combinations of two two-digit numbers.
+* main-program entry point
 *
-* Return: 0 (Success)
+* Description:prints all possible combinations of two two-digit numbers.
+* Return:0
 */
 int main(void)
 {
-int num1, num2;
+int i = 0, j;
 
-for (num1 = 0; num1 <= 99; num1++)
+while (i <= 99)
 {
-for (num2 = num1 + 1; num2 <= 99; num2++)
+j = i;
+
+while (j <= 99)
 {
-putchar(num1 / 10 + '0');
-putchar(num1 % 10 + '0');
+if (j != i)
+{
+putchar((i / 10) + 48);
+putchar((i % 10) + 48);
 putchar(' ');
-putchar(num2 / 10 + '0');
-putchar(num2 % 10 + '0');
+putchar((j / 10) + 48);
+putchar((j % 10) + 48);
 
-if (num1 != 98 || num2 != 99)
+if (i != 98 || j != 99)
 {
 putchar(',');
 putchar(' ');
 }
 }
+++j;
 }
-
+++i;
+}
+putchar('\n');
 return (0);
 }
+
+
