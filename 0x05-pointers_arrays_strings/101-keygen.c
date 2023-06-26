074a121735aa13328a026a297661fbfea9fb5d52
Mato Adrian <adriangekara8@gmail.com>
2023-06-26 16:13:21 -0700

    Create a program that generates random valid passwords for the program 101-crackme. > > > > You are allowed to use the standard library > > You don’t have to pass the betty-style tests (you still need to pass the betty-doc tests) > > man srand, rand, time > > gdb and objdump can help

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,29 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
 /**
- * main - entry point to generate a random valid password
+ * main - entry point generates a random password
  *
- * Return: 0
+ * Return: the generated password
  */
 int main(void)
 {
-char password[7];
-int i, sum = 0;
+char c;
+int x;
 
 srand(time(0));
-
-for (i = 0; i < 7; i++)
+while (x <= 2645)
 {
-password[i] = rand() % 94 + 33;  // Generate random ASCII characters from '!' to '~'
-sum += password[i];
+c = rand() % 128;
+x += c;
+putchar(c);
 }
-
-password[7] = '\0';  // Add null terminator to make it a valid C string
-
-printf("%s\n", password);
-printf("Sum: %d\n", sum);
-
-return 0;
+putchar(2772 - x);
+return (0);
 }
