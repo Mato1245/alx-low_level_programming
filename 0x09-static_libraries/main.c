83957e013f477cae9fc51935e7f5dd27a62e844c
Mato Adrian <adriangekara8@gmail.com>
2023-07-09 14:17:52 -0700

    Create the static library libmy.a containing all the functions listed below:
    int _putchar(char c);
    int _islower(int c);
    int _isalpha(int c);
    int _abs(int n);
    int _isupper(int c);
    int _isdigit(int c);
    int _strlen(char *s);
    void _puts(char *s);
    char *_strcpy(char *dest, char *src);
    int _atoi(char *s);
    char *_strcat(char *dest, char *src);
    char *_strncat(char *dest, char *src, int n);
    char *_strncpy(char *dest, char *src, int n);
    int _strcmp(char *s1, char *s2);
    char *_memset(char *s, char b, unsigned int n);
    char *_memcpy(char *dest, char *src, unsigned int n);
    char *_strchr(char *s, char c);
    unsigned int _strspn(char *s, char *accept);
    char *_strpbrk(char *s, char *accept);
    char *_strstr(char *haystack, char *needle);

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
 #include "main.h"
-
+/**
+ * main - Entry point of the program.
+ *
+ * Return: Always 0 for success.
+ */
 int main(void)
 {
-puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
+puts("\"At the end of the day, my goal was to be the best hacker\"\n"
+	"\t- Kevin Mitnick");
 return (0);
 }
