2068af4aef1b2554242325791772b28ccf3bba31
Mato Adrian <adriangekara8@gmail.com>
2023-08-01 13:49:33 -0700

    Write a function that inserts a new node at a given position.
    Prototype: listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
    where idx is the index of the list where the new node should be added. Index starts at 0
    Returns: the address of the new node, or NULL if it failed
    if it is not possible to add the new node at index idx, do not add the new node and return NULL

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,52 +1,64 @@
 #include "lists.h"
 
 /**
-* insert_nodeint_at_index - function inserts node at given index
-* @head: linked list head address
-* @idx: index to instert node
-* @n: inserted nodes integer value
-* Return: address of inserted node
+* insert_nodeint_at_index - returns the nth node of a linked list
+* @head: pointer to the head of the list
+* @idx: index of the node to be added
+* @n: content of the new node
+*
+* Return: the address of the node
 */
-
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-listint_t *node;
-listint_t *current, *prev;
-unsigned int index;
+listint_t *new_node = NULL;
+listint_t *previous_node = NULL;
+unsigned int i = 0;
 
-current = *head;
-node = (listint_t *) malloc(sizeof(listint_t));
-index = 0;
-
-if (node == NULL)
+new_node = malloc(sizeof(listint_t));
+if (new_node == NULL || idx > listint_len(*head))
 {
-free(node);
+free(new_node);
+return (NULL);
+}
+new_node->n = n;
+new_node->next = NULL;
+while (head != NULL)
+{
+if (i == idx)
+{
+if (i == 0)
+{
+new_node->next = *head;
+*head = new_node;
+return (new_node);
+}
+new_node->next = previous_node->next;
+previous_node->next = new_node;
+return (new_node);
+}
+else if ((i + 1) == idx)
+previous_node = *head;
+head = &((*head)->next);
+i++;
+}
 return (NULL);
 }
 
-node->n = n;
+/**
+* listint_len - counts the number of nodes in a linked list
+* @h: head of the list
+*
+* Return: the number of elements
+*/
+size_t listint_len(const listint_t *h)
+{
+const listint_t *cursor = h;
+size_t count = 0;
 
-do {
-if (current == NULL)
+while (cursor != NULL)
 {
-node->next = *head;
-*head = node;
-return (node);
+count += 1;
+cursor = cursor->next;
 }
-if (current->next != NULL)
-{
-prev = current;
-current = current->next;
-}
-index += 1;
-} while (index != idx);
-
-if (current == NULL)
-{
-free(node);
-return (NULL);
-}
-prev->next = node;
-node->next = current;
-return (node);
+return (count);
 }
