f353b0533d9b3a2211ec4c9383c23aa2a6ef08ae
Mato Adrian <adriangekara8@gmail.com>
2023-08-01 13:04:10 -0700

    Write a function that frees a listint_t list.
    Prototype: void free_listint2(listint_t **head);
    The function sets the head to NULL

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,27 +1,24 @@
 #include "lists.h"
 
-/**
-*free_listint2 - frees a linked list
-*@head: pointer to the head of the list
-*
-*Return: void
-*/
-
 void free_listint2(listint_t **head)
 {
-listint_t *cursor;
-listint_t **temp = head;
+listint_t *pntr;
 
-if (temp != NULL)
+pntr = *head;
+
+if (head == NULL)
+{
+return;
+}
+
+if (pntr != NULL)
 {
 while (*head != NULL)
 {
-cursor = *head;
-free(cursor);
+pntr = *head;
 *head = (*head)->next;
+free(pntr);
 }
-
-*temp = NULL;
-
 }
+*head = NULL;
 }
