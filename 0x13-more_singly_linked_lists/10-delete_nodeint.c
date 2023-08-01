953d94a31cbcfc0c0b1aff8e0580ecdc8d8181a8
Mato Adrian <adriangekara8@gmail.com>
2023-08-01 13:45:45 -0700

    Write a function that deletes the node at index index of a listint_t linked list.
    Prototype: int delete_nodeint_at_index(listint_t **head, unsigned int index);
    where index is the index of the node that should be deleted. Index starts at 0
    Returns: 1 if it succeeded, -1 if it failed

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -1,41 +1,58 @@
 #include "lists.h"
-#include <stdlib.h>
 
 /**
-* delete_nodeint_at_index - Deletes a node at a specified index in a linked list.
-* @head: A double pointer to the head of the linked list.
-* @index: The index of the node to be deleted. Indexing starts from 0.
-* Return: On successful deletion, the function returns 1. If the index is out
-* of range or the list is empty, the function returns -1.
+* delete_nodeint_at_index - deletes a node at an index
+* @head: pointer to the head of the list
+* @index: index of the node to be added
+*
+* Return: the address of the node
 */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
-unsigned int count = 0;
-listint_t *current = *head;
-listint_t *prev = NULL;
+listint_t *old_node = NULL;
+listint_t *previous_node = NULL;
+unsigned int i = 0, list_len = listint_len(*head);
 
-if (*head == NULL)
+if ((index > list_len) || (list_len == 0))
 return (-1);
-
-if (index == 0)
+while (head != NULL)
 {
-*head = (*head)->next;
-free(current);
+if (i == index)
+{
+old_node = *head;
+if (i == 0)
+{
+*head = old_node->next;
+free(old_node);
 return (1);
 }
-while (current != NULL)
-{
-if (count == index)
-{
-prev->next = current->next;
-free(current);
+previous_node->next = old_node->next;
+free(old_node);
 return (1);
 }
-
-prev = current;
-current = current->next;
-count++;
+else if ((i + 1) == index)
+previous_node = *head;
+head = &((*head)->next);
+i++;
+}
+return (-1);
 }
 
-return (-1);
+/**
+* listint_len - counts the number of nodes in a linked list
+* @h: head of the list
+*
+* Return: the number of elements
+*/
+size_t listint_len(const listint_t *h)
+{
+const listint_t *cursor = h;
+size_t count = 0;
+
+while (cursor != NULL)
+{
+count += 1;
+cursor = cursor->next;
+}
+return (count);
 }
